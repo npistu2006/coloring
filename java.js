e7b7b2c4ee7fb9f1b3c758950666cbdcc10aa4fd
npistu <nagyistvanmate20@gmail.com>
2023-11-07 11:29:48 +0100

    Interaktív

--- java.js
+++ java.js
@@ -11,8 +11,47 @@ const boxes = document.querySelectorAll(".box");
 //Végig megyünk az összes div-eken, és rájuk akasztunk egy esemény figyelőt:
 boxes.forEach(element => {
     // Az esemény figyelő a click eseményre indul be: 
+    
     element.addEventListener("click", (event) => {
-        event.target.style.backgroundColor = "red";
+        console.log(event) //eljárás hívás
+        let currentElement = event.target;
+
+        if(currentElement.style.backgroundColor === ""){
+            currentElement.style.backgroundColor = "#ff0000";
+            currentElement.textContent = currentElement.id;
+        }
+        else{
+            currentElement.style.backgroundColor = "";
+            currentElement.textContent = ""; //null
+        }
+
+        currentElement.style.display = "flex"
+        currentElement.style.alignItems = "center";
+        currentElement.style.justifyContent = "center";
+        currentElement.textContent = currentElement.id;
+        
+        
+        
     })
+    
 })
 
+/*
+boxes.forEach( szinez);
+
+function szinez(element){
+    element.addEventListener("click", (event) => {
+        event.target.style.backgroundColor = "red"
+    })
+}
+*/
+
+
+/*
+boxes.forEach(element =>{
+    element.addEventListener("click", hattarszinez); //callBack eljárás (függvény)
+    function hattarszinez(event){
+        event.target.style.backgroundColor = "red";
+    }
+})
+*/
